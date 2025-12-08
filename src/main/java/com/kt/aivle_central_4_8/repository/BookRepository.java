96b55932bb7a3f44e7d915a5e48a8768d3f39d70
hyerim <yhrim9507@gmail.com>
2025-12-08 17:52:38 +0900

    fix: BookRepository fix (#15)

--- src/main/java/com/kt/aivle_central_4_8/repository/BookRepository.java
+++ src/main/java/com/kt/aivle_central_4_8/repository/BookRepository.java
@@ -2,7 +2,6 @@
 
 package com.kt.aivle_central_4_8.repository;
 
-import com.kt.aivle_central_4_8.dto.book.response.BookSummaryDto;
 import com.kt.aivle_central_4_8.entity.BookEntity;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -20,16 +19,6 @@ public interface BookRepository extends JpaRepository<BookEntity, String> {
      *   [0] = BookEntity
      *   [1] = String coverFileEn (예: bookCover.bookUrlDc)
      */
-    // 숫자로 비교했을 때 제일 큰 book_cd 가져오기
-    @Query(
-            value = "SELECT book_cd " +
-                    "FROM book_tb " +
-                    "ORDER BY CAST(SUBSTRING(book_cd, 2) AS UNSIGNED) DESC " +
-                    "LIMIT 1",
-            nativeQuery = true
-    )
-    String findLastBookCd();   // 없으면 null
-
     @Query("""
     SELECT b, c.coverFileEn
     FROM BookEntity b
@@ -61,4 +50,13 @@ public interface BookRepository extends JpaRepository<BookEntity, String> {
             "WHERE (:genre IS NULL OR b.bookGenreFg = :genre)")
     int countBooks(String genre);
 
+    // 숫자로 비교했을 때 제일 큰 book_cd 가져오기
+    @Query(
+            value = "SELECT book_cd " +
+                    "FROM book_tb " +
+                    "ORDER BY CAST(SUBSTRING(book_cd, 2) AS UNSIGNED) DESC " +
+                    "LIMIT 1",
+            nativeQuery = true
+    )
+    String findLastBookCd();   // 없으면 null
 }
