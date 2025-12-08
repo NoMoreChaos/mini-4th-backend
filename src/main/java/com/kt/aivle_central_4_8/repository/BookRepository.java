abc8f32d00b88314c7009011734638feb36101c5
hyerim <yhrim9507@gmail.com>
2025-12-08 15:51:27 +0900

    fix: create book (#2)

--- src/main/java/com/kt/aivle_central_4_8/repository/BookRepository.java
+++ src/main/java/com/kt/aivle_central_4_8/repository/BookRepository.java
@@ -9,30 +9,18 @@ import org.springframework.data.domain.Pageable;
 import java.util.List;
 
 public interface BookRepository extends JpaRepository<BookEntity, String> {
-
-    // 전체 책 권수
     @Query("SELECT COUNT(b) FROM BookEntity b")
     int countAllBooks();
 
-    //  Book + Cover 조인해서 DTO 로 반환
-    @Query("""
-        SELECT new com.kt.aivle_central_4_8.dto.book.response.BookSummaryDto(
-            b.bookCd,
-            b.bookNm,
-            b.bookSummaryDc,
-            b.bookGenreFg,
-            b.bookCreateDt,
-            b.bookModifyDt,
-            c.coverFileEn
-        )
-        FROM BookEntity b
-        LEFT JOIN CoverEntity c
-            ON c.bookCd = b.bookCd
-           AND c.coverSelectYn = true
-        WHERE (:genre IS NULL OR :genre = '' OR b.bookGenreFg = :genre)
-        ORDER BY b.bookCreateDt DESC
-        """)
-    List<BookSummaryDto> findBooks(String genre, Pageable pageable);
+    @Query("SELECT b FROM BookEntity b " +
+            "WHERE (:genre IS NULL OR b.bookGenreFg = :genre) " +
+            "ORDER BY b.bookCreateDt DESC")
+    List<BookEntity> findBooks(String genre, Pageable pageable);
+
+    @Query("SELECT COUNT(b) FROM BookEntity b " +
+            "WHERE (:genre IS NULL OR b.bookGenreFg = :genre)")
+    int countBooks(String genre);
+
 
     // 숫자로 비교했을 때 제일 큰 book_cd 가져오기
     @Query(
