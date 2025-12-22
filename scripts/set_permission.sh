08689977d9d04d1cf3cfaace42848a6ae4653bef
GrabSomeChicken <sj77007983@kangwon.ac.kr>
2025-12-22 17:59:02 +0900

    update prefix

--- scripts/set_permission.sh
+++ scripts/set_permission.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 chmod +x /home/ubuntu/app/*.jar
 chmod +x /home/ubuntu/app/*.sh
+export DB_URL="jdbc:mariadb://aivle5th.ceq2mnp4ry1v.us-east-2.rds.amazonaws.com:3306/aivle4th"
+export DB_ID="admin"
+export DB_PW="5ELvGMmIHxEZTXL9hVBz"
