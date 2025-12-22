7483f3ee4b0e8440503c36f24130c441dd1eb629
GrabSomeChicken <sj77007983@kangwon.ac.kr>
2025-12-22 18:24:27 +0900

    updated prefix

--- scripts/start.sh
+++ scripts/start.sh
@@ -3,4 +3,8 @@ echo "Starting Spring Boot application"
 
 cd /home/ubuntu/app
 
+export DB_URL="jdbc:mariadb://aivle5th.ceq2mnp4ry1v.us-east-2.rds.amazonaws.com:3306/aivle4th"
+export DB_ID="admin"
+export DB_PW="5ELvGMmIHxEZTXL9hVBz"
+
 nohup java -jar *SNAPSHOT.jar > app.log 2>&1 &
